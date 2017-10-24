9732c666f5619afa30f250756d0a4a3b54aa4015
macdarat <makubyz@gmail.com>
2017-10-24 15:20:34 +0100

    Outline for new tests and class

---
+++ BinTree/source/graph.rb
@@ -0,0 +1,9 @@
+require_relative './node.rb'
+
+def add_node
+
+end
+
+def lowest_com_ancestor
+  
+end
---
+++ BinTree/source/test_graph.rb
@@ -0,0 +1,4 @@
+require_relative 'graph.rb'
+require_relative 'node.rb'
+require 'minitest/spec'
+require 'minitest/autorun'
